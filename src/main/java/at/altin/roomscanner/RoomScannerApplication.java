b6dd8722cef2bdbe58960931d5f09d8c59222d0e
altin <kelmendialtin48@gmail.com>
2022-06-10 21:44:53 +0200

    Initial commit

---
+++ src/main/java/at/altin/roomscanner/RoomScannerApplication.java
@@ -0,0 +1,13 @@
+package at.altin.roomscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomScannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomScannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/altin/roomscanner/RoomScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.altin.roomscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
